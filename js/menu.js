e5de0b2f3afaa8ee6574b00e7d2289d4dfcd53ff
xerranox <oficialxerranox@gmail.com>
2022-06-12 20:53:06 +0200

    vStudio

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+let btnMenu = document.querySelector("#btn-menu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+});
---
+++ js/slider.js
@@ -0,0 +1,45 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider-section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const btnLeft = document.querySelector("#btn-Left");
+const btnRight = document.querySelector("#btn-Right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider-section");
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+Next();
+
+setInterval(function(){
+    Next();
+}, 5000);
